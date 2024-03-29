3345d8367dae4f286d5eec6596281c173f637517
abu <abu@gmail.com>
2024-03-29 18:40:05 +0800

    auto commit

--- test/main.go
+++ test/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"xapp/xapp"
 )
 
 func main() {
 	xapp.Init()
-	a := "AT,BE,CY,EE,FI,FR,DE,GR,IE,IT,LV,LT,LU,MT,NL,PT,SK,SI,ES,AE"
-	b := ""
-	x := strings.Contains(a, b)
-	fmt.Println(x)
 	xapp.Run(func() {
-		x := xapp.Redis().Lock("test", 0)
-		x.UnLock()
 	})
 }
